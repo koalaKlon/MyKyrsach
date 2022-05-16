2f58677c0debada9f90e76b67452a861a3efc9ed
koalaKlon <95370656+koalaKlon@users.noreply.github.com>
2022-05-16 23:02:56 +0300

    Add files via upload

--- JS/cur.js
+++
@@ -1,3 +0,0 @@
-onblur="if (!window.__cfRLUnblockHandlers) return false; if(this.value=='') this.value='Поиск';" onfocus="if (!window.__cfRLUnblockHandlers) return false; if(this.value=='Поиск') this.value='';"
-
-onclick="if (!window.__cfRLUnblockHandlers) return false; submit();"

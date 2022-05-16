cd3c5919f9331c2c78092ee92b4537ad65cfbbe7
koalaKlon <95370656+koalaKlon@users.noreply.github.com>
2022-05-16 18:16:12 +0300

    Add files via upload
    Appdate version

---
+++ JS/cur.js
@@ -0,0 +1,3 @@
+onblur="if (!window.__cfRLUnblockHandlers) return false; if(this.value=='') this.value='Поиск';" onfocus="if (!window.__cfRLUnblockHandlers) return false; if(this.value=='Поиск') this.value='';"
+
+onclick="if (!window.__cfRLUnblockHandlers) return false; submit();"
